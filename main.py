b1ff8dd1ee18c7348d9d558950c7219f4175dd90
bruderooo <szymonjacon@gmail.com>
2021-08-10 21:36:44 +0200

    Add usage of DataGenerator in main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import numpy as np
 import pandas as pd
 
+from DataGenerator import DataGenerator
+
 if __name__ == '__main__':
     df = pd.read_csv('data\\training_labels.csv', sep=',').sample(frac=1).set_index('id')
 
     *train, validation, test = np.split(df.index.values, 5)
     partition: dict = {'train': np.array(train).flatten(), 'validation': validation, 'test': test}
     labels: dict = df.to_dict()
+
+    params: dict = {'dim': (32, 32, 32),
+                    'batch_size': 64,
+                    'n_classes': 6,
+                    'n_channels': 1,
+                    'shuffle': True}
+
+    training_generator = DataGenerator(partition['train'], labels, **params)
+    validation_generator = DataGenerator(partition['validation'], labels, **params)
