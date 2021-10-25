9430cde914995de8e5cd8a88ad99d9d1cdc893d7
bruderooo <szymonjacon@gmail.com>
2021-10-25 18:25:53 +0200

    Add callbacks for stopping and multi-gpu

--- main.py
+++ main.py
@@ -1,43 +1,65 @@
+import os
+
 import numpy as np
 import pandas as pd
-from tensorflow import keras
+from tensorflow import keras, distribute
 
 from generators import DataGenerator
-from model import ResNet
+from model.ConvModel import ConvModel
+
+checkpoint_dir = "./ckpt"
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+
+
+def make_or_restore_model():
+    # Either restore the latest model, or create a fresh one
+    # if there is no checkpoint available.
+    checkpoints = [checkpoint_dir + "/" + name for name in os.listdir(checkpoint_dir)]
+    if checkpoints:
+        latest_checkpoint = max(checkpoints, key=os.path.getctime)
+        print("Restoring from", latest_checkpoint)
+        return keras.models.load_model(latest_checkpoint)
+    print("Creating a new model")
+    model = ConvModel()
+    model.compile(
+        optimizer=keras.optimizers.Adam(1e-4),
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=["accuracy"]
+    )
+    return model
+
 
 if __name__ == '__main__':
     df = pd.read_csv('data\\training_labels.csv', sep=',').sample(frac=1).set_index('id')
 
-    # TODO to usuń xd
-    df = df.head(100_000)
-
-    *train, validation, test = np.split(df.index.values, 5)
-    partition: dict = {'train': np.array(train).flatten(), 'validation': validation, 'test': test}
+    *train, validation = np.split(df.index.values, 5)
+    partition: dict = {'train': np.array(train).flatten(), 'validation': validation}
     labels: dict = df.to_dict()['target']
 
-    params: dict = {'dim': (18, 129),
-                    'batch_size': 1024,
+    params: dict = {'dim': (1, 4096),
+                    'batch_size': 64,
                     'n_channels': 3,
                     'shuffle': True}
 
     training_generator = DataGenerator(partition['train'], labels, **params)
     validation_generator = DataGenerator(partition['validation'], labels, **params)
 
-    # shape = (18, 129, 3)
-    # model: keras.models.Model = ResNeXt()
-    model: keras.models.Model = ResNet()
-    model.compile(
-        optimizer=keras.optimizers.Adam(1e-4),
-        loss=keras.losses.BinaryCrossentropy(),
-        metrics=["accuracy"]
-    )
+    strategy = distribute.MirroredStrategy()
+
+    # Open a strategy scope and create/restore the model
+    with strategy.scope():
+        # shape = (1, 4096, 3)
+        model = make_or_restore_model()
 
     history = model.fit(
         x=training_generator,
         validation_data=validation_generator,
-        epochs=5000,
-        verbose=2,
+        epochs=100,
+        verbose=1,
         callbacks=[keras.callbacks.CSVLogger(
             'log.csv', separator=',', append=False
+        ), keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_dir + "/ckpt-{epoch}", save_freq="epoch"
         )]
     )
