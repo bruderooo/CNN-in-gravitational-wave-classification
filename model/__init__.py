b6cc642df5182846d24fd88b3559d69666ad7ff4
bruderooo <szymon.jacon@gmail.com>
2021-11-24 14:53:25 +0100

    Correct __init__ file for model

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,6 @@
+from .ModelWithSummary import Model
 from .ConvBlock import ConvBlock
 from .ConvModel import ConvModel
 from .ConvNeuralNet import ConvNeuralNet
-from .ModelWithSummary import Model
 from .PaperModel import PaperModel
 from .ResNet import ResNet
