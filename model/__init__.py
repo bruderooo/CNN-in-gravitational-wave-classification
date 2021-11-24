b7369edd508ba7ec99322424771b16954cb2ba3b
bruderooo <szymonjacon@gmail.com>
2021-11-24 15:01:33 +0100

    Refactoring working version

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,2 @@
-from .ConvBlock import ConvBlock
-from .ConvModel import ConvModel
 from .ConvNeuralNet import ConvNeuralNet
 from .ModelWithSummary import Model
-from .PaperModel import PaperModel
-from .ResNet import ResNet
